217441a8036b7b13e256d314f3c18b856bb7576c
Arthur <arthur.aba20@gmail.com>
2024-05-15 16:58:10 -0300

    Bosta

--- controllers/cadastroController.js
+++ controllers/cadastroController.js
@@ -1,12 +1,26 @@
 const conn = require('../config/dbConfig');
+const bcrypt = require('bcrypt');
 
 module.exports = {
-    async createUser(){
+    async createUser(req, res) {
         const nome = req.body.nome;
         const email = req.body.email;
         const senha = req.body.senha;
-        const confirmSenha = req.body.confirmSenha;
 
-        
+        try {
+            // Gerar hash da senha
+            const hashedPassword = await bcrypt.hash(senha, 10); // 10 é o custo do hash
+            // Insira o usuário no banco de dados com a senha hash
+            const sql = `INSERT INTO usuarios (nome, email, senha) VALUES ('${nome}', '${email}', '${hashedPassword}')`;
+            conn.query(sql, function (err) {
+                if (err) {
+                    console.log('Erro: ', err);
+                    return;
+                }
+                res.redirect("/")
+            });
+        } catch (error) {
+            console.log('Erro: ', err);
+        }
     }
 }
