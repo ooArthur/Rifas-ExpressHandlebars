271e143a57b2012618d444fd6f8de40c26a8abc2
Arthur.exe <arthur.aba20@gmail.com>
2024-06-03 20:04:05 -0300

    :sparkles: feat: Sistema Finalizado

--- handlebarsHelpers.js
+++ handlebarsHelpers.js
@@ -1,5 +1,11 @@
 const Handlebars = require('handlebars');
 
+// Helper para comparar se dois valores são iguais
+Handlebars.registerHelper('eq', function(a, b) {
+    return a === b;
+});
+
+// Helper para gerar uma range de números
 Handlebars.registerHelper('range', function(start, end) {
     var array = [];
     for (var i = start; i <= end; i++) {
@@ -8,6 +14,7 @@ Handlebars.registerHelper('range', function(start, end) {
     return array;
 });
 
+// Helper para verificar se um bilhete está disponível
 Handlebars.registerHelper('isAvailable', function(billet, bilhetes) {
     for (var i = 0; i < bilhetes.length; i++) {
         if (bilhetes[i].numeroBilhete === billet) {
